c6f9058dbce9a891577611574a4e35ff3c749cdf
juan schenfeld <juan-schenfeld-p@outlook.com>
2023-08-04 14:41:55 -0300

    now use SauceLabs for Selenim Grid and added instructions for github actions

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,19 +1,40 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import pages.HomePage;
 
-public class BaseTest {
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
-    private WebDriver driver;
+public class  BaseTest {
+
+    private RemoteWebDriver driver;
     HomePage homePage;
 
     @BeforeTest
-    void setUp(){
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+    void setUp() throws MalformedURLException {
+        ChromeOptions browserOptions = new ChromeOptions();
+        browserOptions.setPlatformName("Windows 11");
+        browserOptions.setBrowserVersion("latest");
+        Map<String, Object> sauceOptions = new HashMap<>();
+        sauceOptions.put("username", "oauth-Juan-Schenfeld-P-218f9");
+        sauceOptions.put("accessKey", "7f5a33cb-c14b-42d5-8afa-eafa91cc926d");
+        sauceOptions.put("build", "selenium-build-1");
+        sauceOptions.put("name", "general");
+        browserOptions.setCapability("sauce:options", sauceOptions);
+
+
+        URL url = new URL("https://ondemand.us-west-1.saucelabs.com:443/wd/hub");
+        driver = new RemoteWebDriver(url, browserOptions);
+
+//        WebDriverManager.chromedriver().setup();
+//        driver = new ChromeDriver();
         driver.get("https://practice-automation.com/");
 
         homePage = new HomePage(driver);
